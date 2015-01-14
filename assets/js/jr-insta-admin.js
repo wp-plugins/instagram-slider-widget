ca9aab09187205bb2c487aeee6889bbd94cd24f8
jetonr <jetonr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-14 00:46:38 +0000

    Move current version to archive tag 1.2.0
    git-svn-id: https://plugins.svn.wordpress.org/instagram-slider-widget/tags/1.2.0@1067535 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ assets/js/jr-insta-admin.js
@@ -0,0 +1,52 @@
+(function($) {
+	
+	$(document).ready(function($){
+		
+		// Hide Custom Url if image link is not set to custom url
+		$('body').on('change', '.jr-container select[id$="images_link"]', function(e){
+			var images_link = $(this);
+			if ( images_link.val() != 'custom_url' ) {
+				images_link.closest('.jr-container').find('input[id$="custom_url"]').val('').parent().animate({opacity: 'hide' , height: 'hide'}, 200);
+			} else {
+				images_link.closest('.jr-container').find('input[id$="custom_url"]').parent().animate({opacity: 'show' , height: 'show'}, 200);
+			}
+		});
+
+		// Hide Refresh hour if source is wp media library
+		$('body').on('change', '.jr-container select[id$="template"]', function(e){
+			var template = $(this);
+			if ( template.val() == 'thumbs' ) {
+				template.closest('.jr-container').find('.jr-slider-options').animate({opacity: 'hide' , height: 'hide'}, 200);
+				template.closest('.jr-container').find('input[id$="columns"]').closest('p').animate({opacity: 'show' , height: 'show'}, 200);
+			} else {
+				template.closest('.jr-container').find('.jr-slider-options').animate({opacity: 'show' , height: 'show'}, 200);
+				template.closest('.jr-container').find('input[id$="columns"]').closest('p').animate({opacity: 'hide' , height: 'hide'}, 200);
+			}
+		});
+
+		// Hide Refresh hour if source is wp media library
+		$('body').on('change', '.jr-container input:radio[id$="source"]', function(e){
+			var source = $(this);
+			if ( source.val() != 'instagram' ) {
+				source.closest('.jr-container').find('input[id$="refresh_hour"]').closest('p').animate({opacity: 'hide' , height: 'hide'}, 200);
+			} else {
+				source.closest('.jr-container').find('input[id$="refresh_hour"]').closest('p').animate({opacity: 'show' , height: 'show'}, 200);
+			}
+		});		
+
+		// Toggle advanced options
+		$('body').on('click', '.jr-advanced', function(e){
+			e.preventDefault();
+			var advanced_container = $(this).parent().next();
+			
+			if ( advanced_container.is(':hidden') ) {
+				$(this).html('[ - Close ]');
+			} else {
+				$(this).html('[ + Open ]');
+			}
+			advanced_container.toggle();
+		});
+		
+	});
+
+})(jQuery);
